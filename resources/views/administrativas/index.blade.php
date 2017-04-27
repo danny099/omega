ab3eeb72f3b28356b56b0b3bc64af5c97196058f
Markuz1995 <markuz1995@gmail.com>
2017-04-27 10:07:40 -0500

    Agregue el index.blade.php con la opcion logout Agrege el administrativas/index.blade.php con una modal

--- resources/views/administrativas/index.blade.php
+++ resources/views/administrativas/index.blade.php
@@ -4,17 +4,8 @@
 
       <div class="container">
         <div class="col-md-12 well">
-          <a class="btn btn-primary" href="{{ url('administrativas/create') }}"><i class="fa fa-user-plus"></i> Crear Contrato</a>
+          <a class="btn btn-primary" data-toggle="modal" href="{{ url('administrativas/create') }}"><i class="fa fa-user-plus"></i> Crear Contrato</a>
           <div class="box-body">
-            @if(Session::has('message'))
-              <div id="alert">
-                <div class="col-sm-12 hr hr-18 hr-double dotted"></div>
-                <div class="col-sm-4 col-xs-12 col-sm-offset-4 alert alert-{{Session::get('class')}}">
-                  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                  {{Session::get('message')}}
-                </div>
-              </div>
-            @endif
             <table id="example1" class="table table-bordered table-striped">
               <thead>
               <tr>
@@ -39,9 +30,11 @@
                   <td>
                       <a href="{{ route('administrativas.edit', $administrativa->id) }}"><i class="glyphicon glyphicon-pencil"></i></a>
                       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                      <a href="#createModal" data-toggle="modal"><i class="glyphicon glyphicon-eye-open"></i></a>
+                      <a href="" data-toggle="modal" data-target=".bs-example-modal-lg"><i class="glyphicon glyphicon-eye-open"></i></a>
                       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                       <a href="{{ url('deleteadminstrativa') }}/{{ $administrativa->id }}" onClick="javascript: return confirm('Esta seguro de eliminar registro?');"><i class="glyphicon glyphicon-minus-sign"></i></a>
+
+
                   </td>
                   <td></td>
                 </tr>
@@ -61,291 +54,35 @@
             </table>
           </div>
         </div>
-        </div>
+      </div>
 
-        <div class="example-modal">
-          <div class="modal fade bs-example-modal-lg" aria-labelledby="myLargeModalLabel" id="createModal" tabindex="-1" role="dialog" aria-hidden="true">
-            <div class="modal-dialog modal-lg" role="document">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span></button>
-                  <h4 class="modal-title">Default Modal</h4>
-                </div>
-                <div class="modal-body">
-                  <div class="box box-primary">
-                    <div class="box-header with-border">
-                      <center> <h3 class="box-title">Datos del proyecto</h3> </center>
-                    </div>
-                    <!-- /.box-header -->
-                    <!-- form start -->
-                    <form role="form" action="{{ url('administrativas') }}" method="post">
-                      {{ csrf_field() }}
-                      <div class="box-body">
-                        <div class="col-md-4">
-                          <div class="form-group">
-                            <label >Codigo del proyecto:</label>
-                            <input type="text" class="form-control" placeholder="Ingrese codigo" name="codigo">
-                          </div>
-                          <div class="form-group">
-                            <label >nombre del proyecto</label>
-                            <input type="text" class="form-control" placeholder="Ingrese nombre" name="nombre">
-                          </div>
-                          <div class="form-group">
-                            <label >Fecha del contrato:</label>
-                            <input type="date" class="form-control" name="fecha">
-                          </div>
-                          <div class="form-group">
-                            <label >Cliente</label>
-                            <select class="form-control" name="cliente_id">
-                              @foreach($clientes as $cliente)
-                              <option value="{{ $cliente->id }}">{{$cliente->nombre}}</option>
-                              @endforeach
-                            </select>
-                          </div>
-
-                        </div>
-
-                        <div class="col-md-4">
-                          <div class="form-group">
-                            <label >Propietario</label>
-                            <input type="text" class="form-control" placeholder="Ingresa propietario" name="propietario">
-                          </div>
-
-                          <div class="form-group">
-                            <label >Departamento</label>
-                            <select class="form-control" name="departamento">
-
-                            </select>
-                          </div>
-                          <div class="form-group">
-                            <label >Ciudad</label>
-                            <select class="form-control" name="municipio">
-
-                            </select>
-                          </div>
-                          <div class="form-group">
-                            <label >Tipo de zona</label>
-                            <select class="form-control" name="zona">
-
-                            </select>
-                          </div>
-                        </div>
-
-                        <div class="col-md-4">
-
-                          <div class="form-group">
-                            <label >Valor contrato inicial</label>
-                            <input type="number" class="form-control" placeholder= "Ingrese valor" name="contrato_inicial">
-                          </div>
-                          <label >Otro si</label>
-                          <div class="form-group ">
-                            <div class="col-md-11">
-                              <input type="number" class="form-control" placeholder= "Ingrese valor" name="otrosi">
-                            </div>
-
-                            <div class="form-group">
-                              <br>
-                              <br>
-                              <label >Valor contrato final</label>
-                              <input type="number" class="form-control" placeholder= "Ingrese valor" name="contrato_final">
-                            </div>
-                            <div class="form-group">
-                              <label >Plan de pago</label>
-                              <input type="number" class="form-control" placeholder= "Ingrese valor" name="plan_pago">
-                            </div>
-                          </div>
-                        </div>
-                        <hr>
-
-                  </div>
-                  </div>
-
-
-                  <div class="box box-primary">
-                    <div class="box-header with-border">
-                      <center> <h3 class="box-title">Alcance: proceso de transformacion</h3> </center>
-                    </div>
-
-                      <div class="box-body">
-                        <div class="col-md-3">
-                          <div class="form-group">
-                            <center><label >Descripcion</label></center>
-                          </div>
-                          <div class="form-group">
-                            <input type="text" class="form-control" value="Inspecion RETIE proceso de transformacion"  readonly=”readonly” name="descripcion">
-
-                          </div>
-                        </div>
-
-                        <div class="col-md-3">
-                          <div class="form-group">
-                            <center><label >Tipo</label></center>
-                            <select class="form-control" name="tipo">
-
-                            </select>
-                          </div>
-                        </div>
-
-                        <div class="col-md-3">
-                          <div class="form-group">
-                            <center><label >Capacidad</label></center>
-                            <select class="form-control" name="capacidad">
-
-                            </select>
-                          </div>
-                        </div>
-
-                        <div class="col-md-1">
-                          <div class="form-group">
-                            <center><label>Unidad</label></center>
-                            <center>
-                              <input type="text" class="form-control" value="Und"  readonly=”readonly” name="unidad_transformacion">
-                            </center>
-                          </div>
-                        </div>
-
-                        <div class="col-md-1">
-                          <div class="form-group">
-                            <center><label >Cantidad</label></center>
-                            <input type="text" class="form-control" placeholder= "Cantidad" name="cantidad">
-                          </div>
-                        </div>
-
-
-
-                        <div class="col-md-12">
-                          <center> <h4 class="box-title">Alcance: proceso de distribucion</h4> </center>
-                        </div>
-
-                        <div class="col-md-3">
-                          <div class="form-group">
-                            <center><label >Descripcion</label></center>
-                          </div>
-                          <div class="form-group">
-                            <select class="form-control" name="descripcion_dis">
-
-                            </select>
-                          </div>
-                        </div>
-
-                        <div class="col-md-3">
-                          <div class="form-group">
-                            <center><label >Tipo</label></center>
-                            <select class="form-control" name="tipo_dis">
-
-                            </select>
-                          </div>
-                        </div>
-
-                        <div class="col-md-3">
-                          <div class="form-group">
-                            <center><label >Capacidad</label></center>
-                            <select class="form-control" name="capacidad_dis">
-
-                            </select>
-                          </div>
-                        </div>
-
-                        <div class="col-md-1">
-                          <div class="form-group">
-                            <center><label >Unidad</label></center>
-                            <center>
-                              <input type="text" class="form-control" value="Km"  readonly=”readonly” name="unidad_distribucion">
-                            </center>
-                          </div>
-                        </div>
-
-                        <div class="col-md-1">
-                          <div class="form-group">
-                            <center><label >Cantidad</label></center>
-                            <input type="text" class="form-control" placeholder= "Cantidad" name="cantidad_dis">
-                          </div>
-                        </div>
-
-
-
-                        <div class="col-md-12">
-                          <center> <h4 class="box-title">Alcance: proceso de uso final</h4> </center>
-                        </div>
-                        <div class="col-md-3">
-                          <div class="form-group">
-                            <center><label >Descripcion</label></center>
-                          </div>
-                          <div class="form-group">
-                            <select class="form-control"name="descripcion_pu">
-
-                            </select>
-                          </div>
-                        </div>
-
-                        <div class="col-md-3">
-                          <div class="form-group">
-                            <center><label >Tipo</label></center>
-                            <select class="form-control" name="tipo_pu">
-
-                            </select>
-                          </div>
-                        </div>
-
-                        <div class="col-md-3">
-                          <div class="form-group">
-                            <center><label >Capacidad</label></center>
-                            <select class="form-control" name="capacidad_pu">
-
-                            </select>
-                          </div>
-                        </div>
-
-                        <div class="col-md-1">
-                          <div class="form-group">
-                            <center><label >Unidad</label></center>
-                            <center>
-                              <input type="text" class="form-control" value="Und"  readonly=”readonly” name="unidad_pu_final">
-                            </center>
-                          </div>
-                        </div>
-
-                        <div class="col-md-1">
-                          <div class="form-group">
-                            <center><label >Cantidad</label></center>
-                            <input type="text" class="form-control" placeholder= "Cantidad" name="cantidad_pu">
-                          </div>
-                        </div>
-
-                        <div class="col-md-1">
-                          <div class="form-group">
-                            <br>
-                            <a class="btn btn-primary" data-toggle="modal" href="#" style="background-color: #fdea08; border-color:#fdea08;"><i class="glyphicon glyphicon-plus"></i></a>
-                          </div>
-                        </div>
-
-                        <div class="col-md-12">
-                          <center> <h4 class="box-title">Resumen de estado administrativo del proyecto</h4> </center>
-                        </div>
-
-                        <div class="col-md-12">
-                          <textarea name="name" rows="4" cols="250" name="resumen"></textarea>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-default pull-right" data-dismiss="modal">Close</button>
-
-                </div>
-              </div>
-              <!-- /.modal-content -->
-            </div>
-            <!-- /.modal-dialog -->
-          </div>
-          <!-- /.modal -->
-        </div>
-
-
-
+      <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+      Launch demo modal
+      </button>
 @endsection
+
+<!-- Button trigger modal -->
+
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" style="background= #fff;">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 
 @section('scripts')
 <!-- jQuery 2.2.3 -->
