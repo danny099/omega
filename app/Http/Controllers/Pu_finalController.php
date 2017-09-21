b57339f43377d4f5c77887bcdef71d4c70d701c8
danny099 <dalmariv01@gmail.com>
2017-09-21 11:46:53 -0500

    modificacion de pufinales para los datos

--- app/Http/Controllers/Pu_finalController.php
+++ app/Http/Controllers/Pu_finalController.php
@@ -54,9 +54,10 @@ class Pu_finalController extends Controller
              if (!is_null($input['pu_final']['descripcion_pu'][$i]) &&
                  !is_null($input['pu_final']['tipo_pu'][$i]) &&
                 //  !is_null($input['pu_final']['estrato_pu'][$i]) &&
-                 !is_null($input['pu_final']['cantidad_pu'][$i]) &&
-                 !is_null($input['pu_final']['metros_pu'][$i]) &&
-                 !is_null($input['pu_final']['kva_pu'][$i])) {
+                 !is_null($input['pu_final']['cantidad_pu'][$i]))
+                //  !is_null($input['pu_final']['metros_pu'][$i]) &&
+                //  !is_null($input['pu_final']['kva_pu'][$i]))
+                {
 
                    $datos3['descripcion'] = $input['pu_final']['descripcion_pu'][$i];
                    $datos3['tipo'] = $input['pu_final']['tipo_pu'][$i];
@@ -65,13 +66,6 @@ class Pu_finalController extends Controller
                    $datos3['cantidad'] = $input['pu_final']['cantidad_pu'][$i];
                    $datos3['metros'] = $input['pu_final']['metros_pu'][$i];
 
-                   if ($input['pu_final']['kva_pu'][$i] == 0) {
-
-                     $datos3['kva'] = 'Según Plano';
-
-                   }else {
-                     $datos3['kva'] = $input['pu_final']['kva_pu'][$i];
-                   }
 
                    if ($datos3['tipo'] == 'Casa') {
 
@@ -116,6 +110,34 @@ class Pu_finalController extends Controller
 
                    }else {
                      $datos3['estrato'] = null;
+                   }
+
+                   if (isset($input['pu_final']['metros_pu'][$i])) {
+                     if (!empty($input['pu_final']['metros_pu'][$i])) {
+
+                       $datos3['metros'] = $input['pu_final']['metros_pu'][$i];
+
+                     }
+
+                   }else {
+                     $datos3['metros'] = null;
+                   }
+
+                   if (isset($input['pu_final']['kva_pu'][$i])) {
+                     if (!empty($input['pu_final']['kva_pu'][$i])) {
+
+                       if ($input['pu_final']['kva_pu'][$i] == 0) {
+
+                         $datos3['kva'] = 'Según Plano';
+
+                       }else {
+                         $datos3['kva'] = $input['pu_final']['kva_pu'][$i];
+                       }
+
+                     }
+
+                   }else {
+                     $datos3['kva'] = null;
                    }
 
                    $datos3['administrativa_id'] = $request->codigo_proyecto;
