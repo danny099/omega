760d9b880af10f31df44777e36935a7620aa9567
danny099 <dalmariv01@gmail.com>
2017-12-01 16:30:31 -0500

    pdfs

---
+++ resources/views/pdf/show-disDeta.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <table>
+      <tr>
+        <th>Company</th>
+        <th>Contact</th>
+        <th>Country</th>
+      </tr>
+      <tr>
+        <td>Alfreds Futterkiste</td>
+        <td>Maria Anders</td>
+        <td>Germany</td>
+      </tr>
+    </table>
+  </body>
+</html>
