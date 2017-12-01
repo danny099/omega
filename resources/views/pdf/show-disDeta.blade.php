10cdb88a3b2d859b38264106a19b7a761fdb6287
danny099 <dalmariv01@gmail.com>
2017-12-01 17:56:47 -0500

    pdfs

--- resources/views/pdf/show-disDeta.blade.php
+++ resources/views/pdf/show-disDeta.blade.php
@@ -5,17 +5,25 @@
     <title></title>
   </head>
   <body>
-    <table>
-      <tr>
-        <th>Company</th>
-        <th>Contact</th>
-        <th>Country</th>
+    <table border="1">
+      <tr rowspan="2">
+        <th>item</th>
+        <th coslpan="6">descripcion</th>
+        <th colspan="2">aplica</th>
+        <th colspan="2">cumple</th>
+        <th coslpan="4">observaciones</th>
       </tr>
       <tr>
-        <td>Alfreds Futterkiste</td>
-        <td>Maria Anders</td>
-        <td>Germany</td>
+        <th></th>
+        <th></th>
+
+        <th>Si</th>
+        <th>No</th>
+        <th>Si</th>
+        <th>No</th>
+        <th></th>
       </tr>
+
     </table>
   </body>
 </html>
