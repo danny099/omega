079aa883379187df8f101fd1211e2be7485e9ff7
danny099 <dalmariv01@gmail.com>
2017-12-12 14:11:14 -0500

    index

--- resources/views/ncObra/index.blade.php
+++ resources/views/ncObra/index.blade.php
@@ -25,7 +25,7 @@
                   <div class="modal-content">
                     <div class="modal-header">
                       <button type="button" class="close" data-dismiss="modal">&times;</button>
-                      <h4 class="modal-title">Crear reporte de no conformidades</h4>
+                      <h4 class="modal-title">Crear reporte de no conformidades </h4>
                     </div>
                     <div class="modal-body">
                       <form action="{{ url('ncObra/create')}}" method="post">
